5f26a4dbe755d583b7cd2ec549b84c80a4904575
ThuHuynh2809 <124646995+ThuHuynh2809@users.noreply.github.com>
2023-09-22 17:43:49 +0700

    Update Models

--- src/main/java/vn/edu/iuh/fit/week02_lab_huynhminhthu_20010211/App.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.iuh.fit.week02_lab_huynhminhthu_20010211;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-@ApplicationPath()
-public class App extends Application {
-}
